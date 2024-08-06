1260cd2ca1055661b84946f853badbcf06856de8
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2024-08-06 18:58:41 -0300

    chore: update build-route-path to handle hyphenated route parameters

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,10 +1,7 @@
 // /users/:id is a route path that can be used to delete a user by ID.
 export function buildRoutePath(path) {
   const routeParamsRegex = /:([a-zA-Z]+)/g;
-  const pathWithParams = path.replaceAll(
-    routeParamsRegex,
-    "(?<$1>[a-z0-9i_]+)"
-  );
+  const pathWithParams = path.replaceAll(routeParamsRegex, '(?<$1>[a-z0-9\-_]+)');
 
   const pathRegex = new RegExp(`^${pathWithParams}`);
 
